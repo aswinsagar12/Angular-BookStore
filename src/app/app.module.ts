dc0815c9288e76f1de7310693d10443d2f953724
Aswin Sagar <aswinsagar12@gmail.com>
2022-01-15 22:56:27 +0530

    Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Router, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth.module';
+import { LoginComponent } from './auth/components/login/login.component';
+import { SignupComponent } from './auth/components/signup/signup.component';
 import { BookModule } from './book/book.module';
+import { HomeComponent } from './home/components/home/home.component';
 import { HomeModule } from './home/home.module';
+
+
+const routes = [
+  {path: '',component: HomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'signup', component: SignupComponent}
+]
 
 @NgModule({
   // Components in this module.
@@ -15,6 +26,8 @@ import { HomeModule } from './home/home.module';
   imports: [
     BrowserModule,
     AuthModule,
+    // Make module aware of routing path,
+    RouterModule.forRoot(routes),
     HomeModule,
     BookModule
   ],
